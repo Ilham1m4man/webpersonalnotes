1ccecea4c9a9b6542900719b8ec0f2355f14d732
Ilham1m4man <kucingdiki@gmail.com>
2022-10-06 23:47:52 +0700

    Render note list in archive section

--- src/components/ArchivedNoteList.js
+++ src/components/ArchivedNoteList.js
@@ -1,7 +1,27 @@
-/* import React from "react";
+import React from "react";
+import ArchivedNoteItem from "./archived-note-item/ArchivedNoteItem";
 
-function ArchivedNoteList() {
-    
+function ArchivedNoteList({ archivedNote, onArchivedDelete, onUnarchive }) {
+    const renderArchivedNote = archivedNote.map((notes) => {
+        if (notes.archived) {
+            return (
+                <ArchivedNoteItem
+                    key={notes.id}
+                    id={notes.id}
+                    onArchivedDelete={onArchivedDelete}
+                    onUnarchive={onUnarchive}
+                    {...notes} />
+            );
+        }
+        return null;
+    })
+
+    return (
+        <div className="archived-note-list">
+            {renderArchivedNote}
+        </div>
+    )
+
 }
 
-export default ArchivedNoteList; */
+export default ArchivedNoteList;
