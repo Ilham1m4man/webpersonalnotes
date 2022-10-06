3b39d756f78d9cea7a42dcc2b4a5bed6cd16ec74
Ilham1m4man <kucingdiki@gmail.com>
2022-10-06 23:44:54 +0700

    Modified version of NoteItem file

---
+++ src/components/archived-note-item/ArchivedNoteItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ArchivedNoteItemBody from "./ArchivedNoteItemBody";
+import ArchivedNoteItemButtons from "./ArchivedNoteItemButtons";
+
+function ArchivedNoteItem({ id, title, body, createdAt, onArchivedDelete, onUnarchive }) {
+    return (
+        <div className="note-item">
+            <ArchivedNoteItemBody title={title} body={body} createdAt={createdAt} />
+            <ArchivedNoteItemButtons id={id} onArchivedDelete={onArchivedDelete} onUnarchive={onUnarchive} />
+        </div>
+    )
+}
+
+export default ArchivedNoteItem;
