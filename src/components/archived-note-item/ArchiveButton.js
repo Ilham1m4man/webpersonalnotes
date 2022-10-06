0b186ca501b7ce0c23ec6508cbc80aa9bd5c5ba2
Ilham1m4man <kucingdiki@gmail.com>
2022-10-06 23:44:01 +0700

    renaming file

--- src/components/archived-note-item/ArchiveButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ArchiveButton({ id, onArchive }) {
-    return <button className='note-item-archive' onClick={() => onArchive(id)}>Archive</button>
-}
-
-export default ArchiveButton;
