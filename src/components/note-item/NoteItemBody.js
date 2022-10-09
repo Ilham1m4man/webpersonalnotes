224697c92dfcafa2bf044aee7c869909c18f11db
Ilham1m4man <kucingdiki@gmail.com>
2022-10-09 13:46:07 +0700

    Add some class & make title only show 18 chars

--- src/components/note-item/NoteItemBody.js
+++ src/components/note-item/NoteItemBody.js
@@ -2,10 +2,11 @@ import React from "react";
 
 function NoteItemBody({ title, body, createdAt }) {
     return (
-        <div className="note-item-body">
-            <h3 className="note-item-title">{title}</h3>
+        <div className="note-item-body card-body">
+            <h3 className="note-item-title card-title" numberoflines={1} title={title}>
+                {title.length < 18 ? `${title}` : `${title.substring(0, 18)}...`}</h3>
             <p className="note-item-createdAt">{createdAt}</p>
-            <p className="note-item-body">{body}</p>
+            <p className="note-item-body card-text">{body}</p>
         </div>
     );
 }
