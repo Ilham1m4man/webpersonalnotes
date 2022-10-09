51db31aadf23faa99cf5c47fe9156eccc515ec4e
Ilham1m4man <kucingdiki@gmail.com>
2022-10-09 13:47:31 +0700

    Add class & filter for search feature

--- src/components/ArchivedNoteList.js
+++ src/components/ArchivedNoteList.js
@@ -1,23 +1,26 @@
 import React from "react";
 import ArchivedNoteItem from "./archived-note-item/ArchivedNoteItem";
 
-function ArchivedNoteList({ archivedNote, onArchivedDelete, onUnarchive }) {
+function ArchivedNoteList({ searchNote, archivedNote, onArchivedDelete, onUnarchive }) {
     const renderArchivedNote = archivedNote.map((notes) => {
+        const theTitle = notes.title;
         if (notes.archived) {
-            return (
-                <ArchivedNoteItem
-                    key={notes.id}
-                    id={notes.id}
-                    onArchivedDelete={onArchivedDelete}
-                    onUnarchive={onUnarchive}
-                    {...notes} />
-            );
+            if (theTitle.toLowerCase().includes(searchNote.toLowerCase())) {
+                return (
+                    <ArchivedNoteItem
+                        key={notes.id}
+                        id={notes.id}
+                        onArchivedDelete={onArchivedDelete}
+                        onUnarchive={onUnarchive}
+                        {...notes} />
+                );
+            } 
         }
         return null;
     })
 
     return (
-        <div className="archived-note-list">
+        <div className="note-list row justify-content-center col-lg-4 col-md-6 col-sm-12">
             {renderArchivedNote}
         </div>
     )
