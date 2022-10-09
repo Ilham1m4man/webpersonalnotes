67c8fe0e9239b4000da09bbed0828c4cf3e5da40
Ilham1m4man <kucingdiki@gmail.com>
2022-10-09 13:44:32 +0700

    Add class card

--- src/components/note-item/NoteItem.js
+++ src/components/note-item/NoteItem.js
@@ -4,7 +4,7 @@ import NoteItemButtons from "./NoteItemButtons";
 
 function NoteItem({ id, title, body, createdAt, onDelete, onArchive }) {
     return (
-        <div className="note-item">
+        <div className="note-item card">
             <NoteItemBody title={title} body={body} createdAt={createdAt} />
             <NoteItemButtons id={id} onDelete={onDelete} onArchive={onArchive} />
         </div>
