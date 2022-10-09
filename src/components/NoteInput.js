dab5e5fffb1666b7f5e54a24db1145dd55263595
Ilham1m4man <kucingdiki@gmail.com>
2022-10-09 13:50:22 +0700

    Modified to show chars amount left & add some div

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -38,15 +38,27 @@ class NoteInput extends React.Component {
     }
 
     render() {
+        let charCounterElm;
+        if(this.state.charLimit - this.state.title.length <= 10) {
+            charCounterElm = <p className="char-counter d-flex flex-row-reverse red">Character left: {this.state.charLimit - this.state.title.length}</p>
+        } else {
+            charCounterElm = <p className="char-counter d-flex flex-row-reverse">Character left: {this.state.charLimit - this.state.title.length}</p>
+        }
         return (
-            <>
-                <p className="char-counter">Character left: {this.state.charLimit-this.state.title.length}</p>
-                <form className='create-note' onSubmit={this.onSubmitHandler}>
-                    <input type="text" placeholder="Title" value={this.state.title} onChange={this.onTitleChangeHandler} maxLength={this.state.charLimit} />
-                    <input type="text" placeholder="Description" value={this.state.body} onChange={this.onBodyChangeHandler} />
-                    <button type="submit">Create</button>
-                </form>
-            </>
+
+                <div className="row gy-1">
+                    {charCounterElm}
+                    <form className='create-note text-center' onSubmit={this.onSubmitHandler}>
+                        <input type="text" placeholder="Title" value={this.state.title} onChange={this.onTitleChangeHandler} maxLength={this.state.charLimit} required />
+                        <br></br>
+                        <br></br>
+                        <textarea type="text" placeholder="Description" value={this.state.body} onChange={this.onBodyChangeHandler} />
+                        <br></br>
+                        <br></br>
+                        <button type="submit">Create</button>
+                    </form>
+                </div>
+
         )
     }
 }
